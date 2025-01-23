1216e59c43063615001142f8bcc8c146185554fa
engineers_bro <kartikchauhan336@gmail.com>
2025-01-24 03:03:03 +0530

    tested

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://jobgram.ranakartikchauhan.in',
+    origin:'http://jobgram.ranakartikchauhan.in',
     credentials:true
 }
 
