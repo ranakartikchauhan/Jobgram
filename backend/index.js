59a29076e6d6011d46567d64ede802fe3b0c990e
engineers_bro <kartikchauhan336@gmail.com>
2025-01-24 15:46:48 +0530

    fixed routes

--- backend/index.js
+++ backend/index.js
@@ -32,14 +32,6 @@ app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
-
-// Fallback for all other routes to serve the index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
-});
-
-
 // api's
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
@@ -49,6 +41,13 @@ app.get("/test",(req,res)=>{
   res.send("hello working API")
   console.log("test working")
 })
+
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+// Fallback for all other routes to serve the index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+});
 
 
 
